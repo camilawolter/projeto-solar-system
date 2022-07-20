e832eb279b697f33cf08b2770789910901193a8a
Camila Wolter <camila0wolter@gmail.com>
2022-07-20 15:26:05 -0300

    arrumando lint do requisito 2

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 class SolarSystem extends React.Component {
-    render() {
-      return (
-        <div data-testid="solar-system"></div>
-      );
-    }
+  render() {
+    return (
+      <div data-testid="solar-system"></div>
+    );
+  }
 }
   
-  export default SolarSystem;
+export default SolarSystem;
